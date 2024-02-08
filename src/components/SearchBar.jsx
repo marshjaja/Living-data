d87266f48795cfd099d8c72945015030f1d49951
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-02-08 22:04:14 +0000

    removed getData funnction temporarily

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,39 +8,47 @@ function SearchBar() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResult, setSearchResult] = useState(null);
 
-  async function getData() {
-    const url = `https://uk-real-estate-rightmove.p.rapidapi.com/rent/property-to-rent?identifier=REGION%5E1036&search_radius=0.0&term=${searchTerm}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'b3562c4c1dmshc0b7700a9473456p18b30ejsn9e3240b972fe',
+  function handleResponse(response) {
+    searchResult({
+      ready: true,
+    });
+  }
+
+
+
+  const options = {
+    method: 'GET',
+    url: 'https://uk-real-estate-rightmove.p.rapidapi.com/property-to-rent',
+    params: {
+      identifier: 'REGION^1036',
+      search_radius:'0.0',
+    },
+    headers: {
+        'X-RapidAPI-Key': '3d88c66c72mshbac12c1b88b25b9p145ee0jsnc7f761aead10',
         'X-RapidAPI-Host': 'uk-real-estate-rightmove.p.rapidapi.com',
       },
     };
 
     try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      setSearchResult(result);
-
-      console.log(result);
-
+      const response = axios.request(options);
+      console.log(response.data);
+      
       // render result.data array! on change event for user search
     } catch (error) {
       console.error(error);
     }
-    axios.get(url).then(getData);
+    
   }
   // Function to handle input change
-  const handleInputChange = (event) => {
+  function handleInputChange(event) {
     setSearchTerm(event.target.value);
-  };
+  }
 
   // Function to prevent default form submission and call getData
-  const handleSearch = (event) => {
+  function handleSearch(event) {
     event.preventDefault();
     getData();
-  };
+  }
 
   return (
     <div className="d-flex flex-col">
