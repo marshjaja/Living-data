80c065a2a2d16979785e5a3f319bd9d838bfebdc
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-02-10 15:03:13 +0000

    started validation form to check user completes the required fields of the form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,14 +2,38 @@ import React, { useState } from 'react';
 
 function ContactForm() {
 
+    // create state variable that holds the form data
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         subject: '',
         message: '',
     })
+
+    // create state varibale for form errors
    const [formErrors, setFormErrors] = useState({});
    const [submitting, setSubmitting] = useState(false);
+
+//  Update for data as user types
+   const handleChange = (event) => {
+    const {name, value} = event.target;
+    setFormData({
+        ...formData,[name]: value
+    });
+    setFormErrors({
+        ...formErrors,[name]:''
+    })
+   }
+
+
+//    Validation form to check that all inout fields have been completed by user
+   const validateForm = () => {
+    const errors = {};
+    if (!formData.name.trim()){
+        errors.name = 'Name is required';
+    }
+   }
+
   return (
     
   );
