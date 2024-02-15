384a086698b739e231f6dc0f9655053a5b7aead3
ctjoseph1 <79760037+ctjoseph1@users.noreply.github.com>
2024-02-15 10:57:59 +0000

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import aboutImg from '../assets/images/aboutPhoto.jpg';
+import aboutImg from '../assets/images/aboutPhoto2.jpg';
 function About() {
     return (
         <section id="about" className='"block about-block'>
